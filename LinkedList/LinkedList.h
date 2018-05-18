28f0ba8903882ec0b0f6b0fa5d46390c176ac09f
Karolina Petrova <karolinapetrovawork@gmail.com>
2018-05-18 21:00:00 +0300

    Add files via upload

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,40 @@
+#ifndef LINKEDLIST_H_INCLUDED
+#define LINKEDLIST_H_INCLUDED
+
+template<typename T>
+struct Node {
+	T data;
+	Node * next;
+};
+template <typename T>
+class LinkedList {
+public:
+	LinkedList();
+	LinkedList(const LinkedList<T>&);
+	LinkedList & operator=(const LinkedList<T>&);
+	~LinkedList();
+
+	bool empty() const;
+	int Size() const;
+	Node<T> * getAt(int) const;
+	void insertAtBeginning(T);
+	void insertToEnd(T);
+	void insertBefore(T,T); //inserts before some element
+	void insertAfter(T, T); //inserts after some element
+	void removeAt(int);
+	void removeBefore(T);
+	void removeAfter(T);
+	Node<T> * lookup(int) const;
+
+private:
+
+	void Delete();
+	void copy(const LinkedList<T>&);
+
+	Node<T> * root;
+	static int size;
+};
+
+#endif
+
+#pragma once
---
+++ LinkedList/LinkedList2.hpp
@@ -0,0 +1,181 @@
+#include "stdafx.h"
+#include "LinkedList.h"
+#include <iostream>
+template<typename T>
+int LinkedList<T>::size = 0;
+
+template<typename T>
+void LinkedList<T>::copy(const LinkedList<T> & other) {
+	Node * otherRoot = other.root;
+	if (otherRoot) {
+		root = new Node();
+		root->data = other.root->data;
+	}
+	else {
+		root = NULL;
+		return;
+	}
+	Node * currentNode = root;
+	otherRoot = other.root->next;
+	while (!currentNode) {
+		Node *nextNode = new Node();
+		nextNode->data = otherRoot->data;
+		currentNode->next = nextNode;
+		currentNode = nextNode;
+		otherRoot = otherRoot->next;
+	}
+}
+template<typename T>
+void LinkedList<T>::Delete() {
+	Node * node = root;
+	while (!node) {
+		Node * forDelete = node;
+		node = node->next;
+		delete forDelete;
+	}
+}
+template<typename T>
+LinkedList<T>::LinkedList() : root(NULL) {
+	root = new Node();
+}
+template<typename T>
+LinkedList<T>::LinkedList(const LinkedList<T> & other) {
+	copy(other);
+}
+template<typename T>
+LinkedList<T> & LinkedList<T>::operator=(const LinkedList<T> & other) {
+	if (this != &other) {
+		Delete();
+		copy(other);
+	}
+	return *this;
+}
+template<typename T>
+LinkedList<T>::~LinkedList() {
+	Delete();
+}
+template<typename T>
+bool LinkedList<T>::empty() const {
+	if (!root)
+		return true;
+	else return false;
+}
+
+template<typename T>
+int LinkedList<T>::Size() const {
+	return size;
+}
+
+template<typename T>
+Node<T> * LinkedList<T>::lookup(int index) const {
+	Node<T> * currentNode = root;
+	while (currentNode != NULL && index > 0) {
+		currentNode = currentNode->next;
+		--index;
+	}
+	return currentNode;
+}
+
+template<typename T>
+Node<T> * LinkedList<T>::getAt(int index) const {
+	return lookup(index);
+}
+
+template<typename T>
+void LinkedList<T>::insertAtBeginning(T _data) {
+	Node<T>* newNode = new Node();
+	newNode->data = _data;
+
+	newNode->next = root;
+	root = newNode;
+	++size;
+}
+
+template<typename T>
+void LinkedList<T>::insertToEnd(T _data) {
+	Node<T> * newNode = new Node();
+	newNode->data = _data;
+
+	Node<T> * lastNode = lookup(size-1);
+	lastNode->next = newNode;
+	++size;
+}
+
+template<typename T>
+void LinkedList<T>::insertBefore(T _data,T element) {
+	Node<T> * newNode = new Node();
+	newNode->data = _data;
+	Node<T> * nodes = root;
+	int index = 0;
+	while (nodes->data != element) {
+		nodes = nodes->next;
+		++index;
+	}
+	if (index == 0) {
+		--size;
+		insertAtBeginning(_data);
+	}
+	else {
+		Node<T> * nodeBefore = lookup(index);
+		Node<T> * afterNode = nodeBefore->next;
+		nodeBefore->next = newNode;
+		newNode->next = afterNode;
+	}
+	++size;
+}
+template<typename T>
+void LinkedList<T>::insertAfter(T _data,T element) {
+	Node<T> * newNode = new Node();
+	newNode->data = _data;
+	Node<T> * nodes = root;
+	int index = 0;
+	while (nodes->data != element) {
+		nodes = nodes->next;
+		++index;
+	}
+	Node<T> * nodeBefore = lookup(index);
+	Node<T> * afterNode = nodeBefore->next;
+	nodeBefore->next = newNode;
+	newNode->next = afterNode;
+	++size;
+}
+
+template<typename T>
+void LinkedList<T>::removeAt(int index) {
+	Node<T> *getNode = lookup(index-1);
+	if (index > 1) {
+		Node<T> *getPrevious = lookup(index - 2);
+		getPrevious->next = getNode->next;
+		delete getNode;
+	}
+	else {
+		root = root->next;
+	}
+	--size;
+}
+
+template<typename T>
+void::LinkedList<T>::removeBefore(T element) {
+	int index = 0;
+	Node * nodes = root;
+	while (nodes->data != element) {
+		nodes = nodes->next;
+		++index;
+	}
+	removeAt(index);
+}
+
+template<typename T>
+void::LinkedList<T>::removeAfter(T element) {
+	int index = 1;
+	Node<T> * nodes = root;
+	while (nodes->data != element) {
+		nodes = nodes->next;
+		++index;
+	}
+	Node<T> *getNode = lookup(index);
+	Node<T> *getPrevious = lookup(index - 1);
+	getPrevious->next = getNode->next;
+	delete getNode;
+	--size;
+}
